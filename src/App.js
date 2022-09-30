4b7b2aee35255cae3cc7f9349bb826bc7e311cd2
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2022-09-29 22:07:47 -0300

    implementando ternário para virar as perguntas

--- src/App.js
+++ src/App.js
@@ -3,35 +3,46 @@ import imgLogo from './images/logo.png'
 import imgSetaPlay from './images/seta_play.png'
 import perguntas from './perguntas'
 import setaVirar from './images/seta_virar.png'
+import { useState } from 'react';
 
 
 
 function CardPerguntas(props) {
-    const perguntaEstaAberta = []
+    const [perguntaEstaAberta, setperguntaEstaAberta] = useState([])
+    const [respostaEstaAberta, setrespostaEstaAberta] = useState([])
 
-    function AbrirResposta(){
-        
+    function AbrirResposta() {
+        setrespostaEstaAberta([...respostaEstaAberta, props.index])    
     }
-    function AbrirPergunta(){
-        
-        perguntaEstaAberta.push(props.index)
+
+    function AbrirPergunta() {
+
+        setperguntaEstaAberta([...perguntaEstaAberta, props.index])
         console.log(perguntaEstaAberta)
     }
 
     return (
         <>
-        <div className='pergunta-fechada'>
-            <p>Pergunta {props.index + 1}</p>
-            <img src={imgSetaPlay} alt='' onClick={AbrirPergunta}/>
-        </div>
-        
-        <div className='pergunta-aberta'>
-            {props.Q}
-            <img src={setaVirar} alt='' onClick={AbrirResposta}/>
-        </div>
-        <div className='resposta-aberta'>
-            {props.R}
-        </div>
+            
+
+            {perguntaEstaAberta.includes(props.index) ? respostaEstaAberta.includes(props.index) ? 
+            
+            <div className='resposta-aberta'>
+                <p>{props.R}</p>
+            </div>
+            :
+            <div className='pergunta-aberta'>
+                {props.Q}
+                <img src={setaVirar} alt='' onClick={AbrirResposta} />
+            </div>     
+            :
+            <div className='pergunta-fechada'>
+                <p>Pergunta {props.index + 1}</p>
+                <img src={imgSetaPlay} alt='' onClick={AbrirPergunta} />
+            </div>}
+
+
+            
         </>
     )
 }
@@ -44,8 +55,8 @@ export default function App() {
                 <h1>ZapRecall</h1>
             </div>
 
-            {perguntas.map((p, index) => <CardPerguntas Q={p.Q}R={p.R} index={index} p={p}/>)}
-            
+            {perguntas.map((p, index) => <CardPerguntas Q={p.Q} R={p.R} index={index} p={p} />)}
+
 
         </div>
     )
